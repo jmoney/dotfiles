9cd1979b229931bc74fc60a7122215f41d4ad0c8
Jonathan Monette <1157041+jmoney8080@users.noreply.github.com>
2019-09-29 16:15:14 -0500

    Remove cloudsearch

--- zsh/zsh-homebrew.zsh
+++ zsh/zsh-homebrew.zsh
@@ -3,6 +3,5 @@
 # HomeBrew
 if [ -f /usr/local/bin/brew ] ; then
     [ -d $(/usr/local/bin/brew --prefix coreutils)/libexec/gnubin ] && PATH=$(/usr/local/bin/brew --prefix coreutils)/libexec/gnubin:$PATH
-    [ -d $(/usr/local/bin/brew --prefix aws-cloudsearch) ] && export CS_HOME=$(/usr/local/bin/brew --prefix aws-cloudsearch)
 fi
 
