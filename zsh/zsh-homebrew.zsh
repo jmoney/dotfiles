85f1d7887da06cc5e4cdda18281441c1f23faeb8
Jonathan Monette <jon.monette@gmail.com>
2022-06-21 10:52:24 -0500

    Use prefix in zsh-homebrew

--- zsh/zsh-homebrew.zsh
+++ zsh/zsh-homebrew.zsh
@@ -2,6 +2,6 @@
 
 # HomeBrew
 if [ -f /usr/local/bin/brew ] ; then
-    [ -d $(/usr/local/bin/brew --prefix coreutils)/libexec/gnubin ] && PATH=$(/usr/local/bin/brew --prefix coreutils)/libexec/gnubin:$PATH
+    [ -d $(brew --prefix coreutils)/libexec/gnubin ] && PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
 fi
 
